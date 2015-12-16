b0bc22bbf4b290e53d1a4b5cf2a033817156b1fe
Javier <javier.v.m.1994@gmail.com>
2015-12-15 23:20:25 -0600

    Aqui es el punto en donde puede cambiar la base de datos, altere el de config para que corra en mi compu pero tenemos que regresar el pswd al correcto, el archivo de crea.php ya crea el main.js con colores

--- scripts/main.js
+++ scripts/main.js
@@ -10,135 +10,134 @@ function initialize() {
 
   var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
-	//---------PARTE GENERADA--------//
-		
-  var locacionEvento1 = new google.maps.LatLng(32.525000, -117.016700);
+  map.set("styles", [
+    {
+        "featureType": "administrative",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#444444"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 45
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#46bcec"
+            },
+            {
+                "visibility": "on"
+            }
+        ]
+    }
+]);
 
-  var locacionEvento1Marker = new google.maps.Marker({
-      position: locacionEvento1,
-      map: map,
-      title:"Evento1",
-      //icon: image
-  });
+  var uspolice = {
+    path: google.maps.SymbolPath.CIRCLE,
+    strokeOpacity: 0,
+    fillColor:"#2d7bb9",
+    fillOpacity: 1,
+    scale: 9,
+  };
+  var mxpolice = {
+    path: google.maps.SymbolPath.CIRCLE,
+    strokeOpacity: 0,
+    fillColor:"#191919",
+    fillOpacity: 1,
+    scale: 9,
+  };
 
-  var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Prueba1</h1>'+
-      '<div id=bodyContent>'+
-      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      'sandstone rock formation in the southern part of the '+
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-      'south west of the nearest large town, Alice Springs; 450&#160;km '+
-      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-      'Aboriginal people of the area. It has many springs, waterholes, '+
-      'rock caves and ancient paintings. Uluru is listed as a World '+
-      'Heritage Site.</p>'+
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-      '(last visited June 22, 2009).</p>'+
-      '</div>'+
-      '</div>';
+  var narc = {
+    path: google.maps.SymbolPath.CIRCLE,
+    strokeOpacity: 0,
+    fillColor:"#c62626",
+    fillOpacity: 1,
+    scale: 9,
+  };
 
-  var infoEvento1 = new google.maps.InfoWindow({
-    content: contentString
-  });
+  var borderpatrol = {
+    path: google.maps.SymbolPath.CIRCLE,
+    strokeOpacity: 0,
+    fillColor:"#3da307",
+    fillOpacity: 1,
+    scale: 9,
+  };
 
+  var other = {
+    path: google.maps.SymbolPath.CIRCLE,
+    strokeOpacity: 0,
+    fillColor:"#eab02f",
+    fillOpacity: 1,
+    scale: 9,
+  };
 
-  locacionEvento1Marker.addListener('click', function() {
-    infoEvento1.open(map, locacionEvento1Marker);
-  });
+  
 
+  //---------PARTE GENERADA--------//
+    
 
-	
-  var locacionEvento2 = new google.maps.LatLng(32.366101, -117.061755);
-
-  var locacionEvento2Marker = new google.maps.Marker({
-      position: locacionEvento2,
-      map: map,
-      title:"Evento2",
-      //icon: image
-  });
-
-  var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Prueba2</h1>'+
-      '<div id=bodyContent>'+
-      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      'sandstone rock formation in the southern part of the '+
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-      'south west of the nearest large town, Alice Springs; 450&#160;km '+
-      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-      'Aboriginal people of the area. It has many springs, waterholes, '+
-      'rock caves and ancient paintings. Uluru is listed as a World '+
-      'Heritage Site.</p>'+
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-      '(last visited June 22, 2009).</p>'+
-      '</div>'+
-      '</div>';
-
-  var infoEvento2 = new google.maps.InfoWindow({
-    content: contentString
-  });
-
-
-  locacionEvento2Marker.addListener('click', function() {
-    infoEvento2.open(map, locacionEvento2Marker);
-  });
-
-
-	
-  var locacionEvento3 = new google.maps.LatLng(31.761878, -106.485022);
-
-  var locacionEvento3Marker = new google.maps.Marker({
-      position: locacionEvento3,
-      map: map,
-      title:"Evento3",
-      //icon: image
-  });
-
-  var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">Prueba3</h1>'+
-      '<div id=bodyContent>'+
-      '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      'sandstone rock formation in the southern part of the '+
-      'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-      'south west of the nearest large town, Alice Springs; 450&#160;km '+
-      '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-      'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-      'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-      'Aboriginal people of the area. It has many springs, waterholes, '+
-      'rock caves and ancient paintings. Uluru is listed as a World '+
-      'Heritage Site.</p>'+
-      '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-      'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-      '(last visited June 22, 2009).</p>'+
-      '</div>'+
-      '</div>';
-
-  var infoEvento3 = new google.maps.InfoWindow({
-    content: contentString
-  });
-
-
-  locacionEvento3Marker.addListener('click', function() {
-    infoEvento3.open(map, locacionEvento3Marker);
-  });
-
-
-	
-
-	//---------FIN PARTE GENERADA--------//
-		
-		google.maps.event.trigger(map, 'resize'); 
+  //---------FIN PARTE GENERADA--------//
+    
+    google.maps.event.trigger(map, 'resize'); 
 };
 
 google.maps.event.addDomListener(window, 'load', initialize);
