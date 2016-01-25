fb1a5dc2acd59b081b9dcd8076254e349579ad31
Javier <javier.v.m.1994@gmail.com>
2016-01-25 16:44:31 -0600

    Ya estan los selects dinamicos

--- editar/mcomptext.php
+++ editar/mcomptext.php
@@ -148,15 +148,139 @@ if(isset($_POST['Mod'])){ //CHECAR SI PICO MODIFICAR NOTICIA11	//VARIABLES LIMIT
 			echo '<form action="" method="post">
 				  <input type="hidden" name="id" value="'.$id.'">
 				  <div><input type="text" name="Titulo" value="'.$Titulo.'"></div>
-				  <div><input type="text" name="NomPerp" value="'.$NomPerp.'"></div>
-				  <div><input type="text" name="Grupo" value="'.$Grupo.'"></div>
+				  <div><input type="text" name="NomPerp" value="'.$NomPerp.'"></div>';
+				  
+				  //<div><input type="text" name="Grupo" value="'.$Grupo.'">
+
+				  //-------------------SELECT GRUPO----------//
+				  $q0 = "SELECT Grupo FROM Grupo WHERE Grupo != '".$Grupo."' GROUP BY Grupo ORDER BY idGrupo;"; //SELECCIONO TODO LO CONTRARIO A LO QUE MANDA EL USUARIO
+
+				  $ress = mysql_query($q0); //EJECUTO QUERY
+
+
+				  mysql_error($ress);
+
+				  //echo $q0;
+
+				  echo '<select name = Grupo>
+				  			<option>'.$Grupo.'</option>'; //EGREGO AL SELECT EL GRUPO QUE ES
+				  		
+
+				  while($row = mysql_fetch_assoc($ress)){
+
+				  		$GrupoS = $row['Grupo'];
+
+				  		echo '<option>'.$GrupoS.'</option>'; //AGREGO CAMPO AL SELECT YA CON LAS DEMAS COSAS
+
+				  }
+
+				  mysql_free_result($ress);
+
+				  echo '</select>';
+
+				  //------------FIN SELECT GRUPO--------------------//
+
+				  echo '</div>
 				  <div><input type="text" name="SubG" value="'.$SubG.'"></div>
 				  <div><input type="text" name="NumVictimas" value="'.$NumVictimas.'"></div>
 				  <div><input type="text" name="Fecha" value="'.$Fecha.'"></div>
-				  <div><input type="text" name="Pais" value="'.$Pais.'"></div>
-				  <div><input type="text" name="Estado" value="'.$Estado.'"></div>
-				  <div><input type="text" name="Municipio" value="'.$Municipio.'"></div>
-				  <div><input type="text" name="Categoria" value="'.$Categoria.'"></div>
+				  <div>';
+				  
+
+				  //-------------------SELECT PAIS----------//
+				  $q0 = "SELECT Nombre FROM Pais WHERE Nombre != '".$Pais."';"; //SELECCIONO TODO LO CONTRARIO A LO QUE MANDA EL USUARIO
+
+				  $ress = mysql_query($q0); //EJECUTO QUERY
+
+
+				  mysql_error($ress);
+
+				  //echo $q0;
+
+				  echo '<select name = Pais>
+				  			<option>'.$Pais.'</option>'; //EGREGO AL SELECT EL GRUPO QUE ES
+				  		
+
+				  while($row = mysql_fetch_assoc($ress)){
+
+				  		$PaisS = $row['Nombre'];
+
+				  		echo '<option>'.$PaisS.'</option>'; //AGREGO CAMPO AL SELECT YA CON LAS DEMAS COSAS
+
+				  }
+
+				  mysql_free_result($ress);
+
+				  echo '</select>';
+
+				  //------------FIN SELECT PAIS--------------------//
+
+
+				  echo '</div>
+				  <div>';
+
+
+				  //-------------------SELECT ESTADO----------//
+				  $q0 = "SELECT Nombre FROM Estado WHERE Nombre != '".$Estado."';"; //SELECCIONO TODO LO CONTRARIO A LO QUE MANDA EL USUARIO
+
+				  $ress = mysql_query($q0); //EJECUTO QUERY
+
+
+				  mysql_error($ress);
+
+				  //echo $q0;
+
+				  echo '<select name = Estado>
+				  			<option>'.$Estado.'</option>'; //EGREGO AL SELECT EL GRUPO QUE ES
+				  		
+
+				  while($row = mysql_fetch_assoc($ress)){
+
+				  		$EstadoS = $row['Nombre'];
+
+				  		echo '<option>'.$EstadoS.'</option>'; //AGREGO CAMPO AL SELECT YA CON LAS DEMAS COSAS
+
+				  }
+
+				  mysql_free_result($ress);
+
+				  echo '</select>';
+
+				  //------------FIN SELECT ESTADO--------------------//
+
+				  echo '</div>
+				  <div><input type="text" name="Municipio" value="'.$Municipio.'"></div>';
+				  
+				  
+
+				  //-------------------SELECT CATEGORIA----------//
+				  $q0 = "SELECT Categora FROM TiposViolencia WHERE Categora != '".$Categoria."';"; //SELECCIONO TODO LO CONTRARIO A LO QUE MANDA EL USUARIO
+
+				  $ress = mysql_query($q0); //EJECUTO QUERY
+
+
+				  mysql_error($ress);
+
+				  //echo $q0;
+
+				  echo '<select name = Categoria>
+				  			<option>'.$Categoria.'</option>'; //EGREGO AL SELECT EL GRUPO QUE ES
+				  		
+
+				  while($row = mysql_fetch_assoc($ress)){
+
+				  		$CategoriaS = $row['Categora'];
+
+				  		echo '<option>'.$CategoriaS.'</option>'; //AGREGO CAMPO AL SELECT YA CON LAS DEMAS COSAS
+
+				  }
+
+				  mysql_free_result($ress);
+
+				  echo '</select>';
+
+				  //------------FIN SELECT CATEGORIA--------------------//
+				  echo '</div>
 				  <div><input type="text" name="lg" value="'.$lg.'"></div>
 				  <div><input type="text" name="lt" value="'.$lt.'"></div>
 				  <div><textarea name="Desc">'.$Desc.'</textarea></div>
