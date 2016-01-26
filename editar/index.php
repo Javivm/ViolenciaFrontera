cb1740446e32bde23552d3a8459ba6fc0c2a8bd4
Rodrigo Salmeron <Salmeron@Echo-2.local>
2016-01-25 20:45:14 -0600

    live

--- editar/index.php
+++ editar/index.php
@@ -11,19 +11,9 @@
 		Buscador
 	</div>
 	<div>
-		<form method="post" action="modifica.php">
 		<br>La noticia incluye los apartados de Titulo, Nombre del Perpetrador, Numero de Victimas, Fecha, Estado donde ocurrio, Municipio donde ocurrio, Pais donde ocurriuo, Descripcion, El texto de la noticia, longitud y latitud<br>
 		<br>Que campo desea modificar<br><br>
-		<select name="Campo">
-			<option>Evento</option>
-			<option>Grupo</option>
-			<option>Estado</option>
-			<option>Municipio</option>
-			<option>Tipo de Violencia</option>
-		</select><br><br>
-		<input type='hidden' value='0' name="hidden">
-		<input type="submit" class="buttonsubmit" value="Guardar cambios">
-		</form>
+		<?php include('modifica.php'); ?>
 	</div>
 </main>
 
