ca151af55b3ed3339d52d49d72c837125bef313c
Rodrigo Salmeron <Salmeron@Echo-2.local>
2016-01-27 21:30:54 -0600

    listo

--- inc/config.php
+++ inc/config.php
@@ -2,11 +2,3 @@
 
 	define("BASE_URL","/");
 	define("ROOT_PATH",$_SERVER["DOCUMENT_ROOT"] . "/");
-
-	/**
- 	* Server configuration
- 	*/
-	$cfgServer['host']	= 'localhost';	
-	$cfgServer['user']	= 'root';	
-	$cfgServer['password']	= '3l#p1ngu1no';	 //CAMBIA EL PASSWORD POR EL DEL SERVIDOR //
-	$cfgServer['dbname']	= 'ViolenciaFrontera';	
